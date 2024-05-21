854471d4c0fcc78046a04cd7ce5fe84a97104e36
Bii20 <leocheruiyot.bc@gmail.com>
2024-05-21 15:29:05 +0300

    add a condition to check if the main class is invoked

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return'hello jokerssss111'
+    return'hello joker'
 
+if __name__ == '__main__':
 
-app.run(host='0.0.0',port=5001,debug=True)    
+    app.run(host='0.0.0',port=5001,debug=True)    
